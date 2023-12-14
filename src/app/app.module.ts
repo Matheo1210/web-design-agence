410c478e262dd614d7f52e6b29bcb6ff3b342403
Matheo Hanss <matheo67450@gmail.com>
2023-12-14 19:47:50 +0100

    amélioration de la structures

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,19 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
 import { LegalesComponent } from './legales/legales.component';
 import { NavComponent } from './nav/nav.component';
 import { ProjetComponent } from './projet/projet.component';
 
 @NgModule({
-  declarations: [AppComponent, LegalesComponent, NavComponent, ProjetComponent],
+  declarations: [
+    AppComponent,
+    LegalesComponent,
+    NavComponent,
+    ProjetComponent,
+    FooterComponent,
+  ],
   imports: [BrowserModule, ReactiveFormsModule],
   bootstrap: [AppComponent],
 })
